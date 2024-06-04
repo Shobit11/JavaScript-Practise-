051abe6ca54945b57f815c6e7616e4ea165afbd1
Shobit11 <143055988+Shobit11@users.noreply.github.com>
2024-06-04 17:56:51 +0000

    iife

---
+++ 02_basics/IIFE.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// There are two method to use this methods
+
+// 1st Method
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+
+// 2nd Method
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
