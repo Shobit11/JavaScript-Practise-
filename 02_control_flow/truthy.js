70253b560e80fdf2ee5f8ee4638324c74dcd66c6
Shobit11 <143055988+Shobit11@users.noreply.github.com>
2024-06-06 19:33:51 +0000

    truthy

---
+++ 02_control_flow/truthy.js
@@ -0,0 +1,47 @@
+const userEmail = []      // empty array will always be a true value
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+//****************** How to check a case having an array  */
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+//***************  How to check a case having a object */
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20    // ************ Null operator always check for NULL or UNDEFINED  values ******
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
