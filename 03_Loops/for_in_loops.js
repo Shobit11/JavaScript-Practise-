7d7defbb5ad0fe855bf59b3fbb0858984b172aaf
Shobit11 <143055988+Shobit11@users.noreply.github.com>
2024-06-07 15:06:25 +0000

    for in loops.

---
+++ 03_Loops/for_in_loops.js
@@ -0,0 +1,28 @@
+// **************************** for in loops. ****************************************
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }                         **************8 This wil not itteratre the map , it will give the blank result, we have to use for of to itterate the map function ********************
